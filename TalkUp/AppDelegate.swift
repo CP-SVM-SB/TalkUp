e3905eaef05920565a6f5ccc8971d5bc6a853a39
Savannah McCoy <savannahmccoy@Savannahs-MacBook-Pro.local>
2017-04-19 02:06:11 -0400

    pushing sb

--- TalkUp/AppDelegate.swift
+++ TalkUp/AppDelegate.swift
@@ -38,8 +38,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       
       return true
     }
-    
-  
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -60,8 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        
-        
+        print("app will termininate")
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
